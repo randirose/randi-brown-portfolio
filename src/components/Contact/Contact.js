74b95f7d2fb2773709e3eea5d417ab22454d4cff
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2024-01-01 18:12:59 -0800

    changing contact page, added new resume to resume page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,131 +1,149 @@
-import { useState } from "react";
+// import { useState } from "react";
 import './Contact.css'
+import './Resume.css'
 
-// checks for a valid email using regex
-import { validateEmail } from "../../utils/validateEmail";
+export default function Contact() {
+    return (
+        <>
+        <h1 className="res-header">Contact me using one of the methods below:</h1>
+        
+  <div className='row d-flex justify-content-around m-2'>
+<div className="card-body res-body">
+        <li>email w icon</li>
+        <li>github w icon</li>
+        <li>number w icon</li>
+        <li>github link w icon</li>
+  </div>
+</div>
 
-function Contact() {
-  // Create state variables and set initial value to empty string
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
 
-  const handleInput = (e) => {
-    // this function grabs value from wherever the user's cursor is to display inside the input by setting that state
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, name, and message
-    if (inputType === "email") {
-      setEmail(inputValue);
-    } else if (inputType === "name") {
-      setName(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    if (e.target.name === 'email') {
-        const isValid = validateEmail(e.target.value);
-        if (!isValid) {
-          setErrorMessage('Your email is invalid.');
-        } else {
-          setErrorMessage('');
-        }
-      } else {
-        if (!e.target.value.length) {
-          setErrorMessage(`A ${e.target.name} is required.`);
-        } else {
-          setErrorMessage('');
-        }
-      }
-
+</>
+    );
   }
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
+// // checks for a valid email using regex
+// import { validateEmail } from "../../utils/validateEmail";
 
-    if (name && message && validateEmail(email)) {
-            // clear input after user hits submit
-        setName("");
-        setMessage("");
-        setEmail("");
-        setErrorMessage("");
-        setSuccessMessage("Your contact info was submitted!");
-        return;
-    }
+// function Contact() {
+//   // Create state variables and set initial value to empty string
+//   const [email, setEmail] = useState("");
+//   const [name, setName] = useState("");
+//   const [message, setMessage] = useState("");
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const [successMessage, setSuccessMessage] = useState("");
+
+//   const handleInput = (e) => {
+//     // this function grabs value from wherever the user's cursor is to display inside the input by setting that state
+//     const { target } = e;
+//     const inputType = target.name;
+//     const inputValue = target.value;
+
+//     // Based on the input type, we set the state of either email, name, and message
+//     if (inputType === "email") {
+//       setEmail(inputValue);
+//     } else if (inputType === "name") {
+//       setName(inputValue);
+//     } else {
+//       setMessage(inputValue);
+//     }
+//   };
+
+//   const handleInputChange = (e) => {
+//     if (e.target.name === 'email') {
+//         const isValid = validateEmail(e.target.value);
+//         if (!isValid) {
+//           setErrorMessage('Your email is invalid.');
+//         } else {
+//           setErrorMessage('');
+//         }
+//       } else {
+//         if (!e.target.value.length) {
+//           setErrorMessage(`A ${e.target.name} is required.`);
+//         } else {
+//           setErrorMessage('');
+//         }
+//       }
+
+//   }
+
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (name && message && validateEmail(email)) {
+//             // clear input after user hits submit
+//         setName("");
+//         setMessage("");
+//         setEmail("");
+//         setErrorMessage("");
+//         setSuccessMessage("Your contact info was submitted!");
+//         return;
+//     }
 
 
-  };
+//   };
 
-  return (
-    <section className="contact container m-3">
-      <div className="row d-flex">
+//   return (
+//     <section className="contact container m-3">
+//       <div className="row d-flex">
   
 
-        {/* contact form section  */}
-        <div className="contact-form col-12">
-          <h3>Send Me a Message</h3>
-          <form className="form">
-            {/* Name */}
-            <label for="contact-label">Fill out the fields below and I'll get back to you ASAP!</label>
-            <input
-              className="form-control mt-2 mb-2"
-              value={name}
-              name="name"
-              onBlur={handleInputChange}
-              onChange={handleInput}
-              type="text"
-              id="contact-name"
-              placeholder="Your Name"
-            />
+//         {/* contact form section  */}
+//         <div className="contact-form col-12">
+//           <h3>Send Me a Message</h3>
+//           <form className="form">
+//             {/* Name */}
+//             <label for="contact-label">Fill out the fields below and I'll get back to you ASAP!</label>
+//             <input
+//               className="form-control mt-2 mb-2"
+//               value={name}
+//               name="name"
+//               onBlur={handleInputChange}
+//               onChange={handleInput}
+//               type="text"
+//               id="contact-name"
+//               placeholder="Your Name"
+//             />
 
-            {/* Email */}
+//             {/* Email */}
 
-            <input
-              className="form-control mt-2 mb-2"
-              value={email}
-              name="email"
-              onBlur={handleInputChange}
-              onChange={handleInput}
-              type="email"
-              id="contact-email"
-              placeholder="Your Email"
-            />
+//             <input
+//               className="form-control mt-2 mb-2"
+//               value={email}
+//               name="email"
+//               onBlur={handleInputChange}
+//               onChange={handleInput}
+//               type="email"
+//               id="contact-email"
+//               placeholder="Your Email"
+//             />
 
-            {/* Message */}
-            <textarea
-              className="form-control mt-2 mb-2"
-              value={message}
-              name="message"
-              onBlur={handleInputChange}
-              onChange={handleInput}
-              type="message"
-              id="contact-message"
-              placeholder="Send Me a Message!"
-            />
-            <button className="btn btn-secondary btn-contact-submit mt-2" type="button" onClick={handleFormSubmit}>
-              Submit
-            </button>
-          </form>
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        {successMessage && (
-          <div>
-            <p className="success-text">{successMessage}</p>
-          </div>
-        )}
-      </div>
-    </section>
-  );
-}
-
-export default Contact;
+//             {/* Message */}
+//             <textarea
+//               className="form-control mt-2 mb-2"
+//               value={message}
+//               name="message"
+//               onBlur={handleInputChange}
+//               onChange={handleInput}
+//               type="message"
+//               id="contact-message"
+//               placeholder="Send Me a Message!"
+//             />
+//             <button className="btn btn-secondary btn-contact-submit mt-2" type="button" onClick={handleFormSubmit}>
+//               Submit
+//             </button>
+//           </form>
+//         </div>
+//         {errorMessage && (
+//           <div>
+//             <p className="error-text">{errorMessage}</p>
+//           </div>
+//         )}
+//         {successMessage && (
+//           <div>
+//             <p className="success-text">{successMessage}</p>
+//           </div>
+//         )}
+//       </div>
+//     </section>
+//   );
+// }
