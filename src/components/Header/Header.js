2ca77dc6548319a16d6ad5ea31b7b0816e53025a
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2024-01-01 18:04:47 -0800

    fixed header to top and edited header text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header({ currentTab, handlePageChange }) {
 
   return (
-    <header className="custom-header flex-row px-1" >
+    <header className="custom-header flex-row px-1 fixed-top" >
 
         <Nav
          currentTab={currentTab}
@@ -13,13 +13,12 @@ function Header({ currentTab, handlePageChange }) {
 
         <div
         className='p-5 text-center bg-image'
-        style={{ backgroundImage: "url('./hero2.png')", height: 600, width: '100%' }}
+        style={{ backgroundImage: "url('./hero2.png')", height: 300, width: '100%' }}
       >
           <div className='d-flex justify-content-center align-items-center h-100'>
             <div className='text-dark'>
               <h1 className='mb-3'>Welcome to Randi Brown's Portfolio</h1>
               <h4 className='mb-3'>—Full-Stack Web Developer—</h4>
-              <p>Check out my Portfolio of work, my Resume and my skills, and send me a message on my Contact page. <br />—<br />Visit my GitHub or LinkedIn Profiles, or reach out to my email via the links in the footer!</p>
             </div>
           </div>
         </div>
