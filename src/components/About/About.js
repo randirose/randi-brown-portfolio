815e6cb8e5d3a61b5bb6006819ba40237ea8f140
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2024-01-02 14:32:13 -0800

    updated margins, links styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,9 +8,9 @@ export default function About() {
       <img src="./me.JPG" className="img-fluid rounded-start rounded-end" alt="Randi at Grand Tetons"></img>
     </div>
     <div className="col-md-8">
-      <div className="card-body">
+      <div className="card-body about-body">
         <h5 className="card-title about-title">About Me</h5>
-        <p className="card-text about-text">Hi! My name is Randi and I'm a full-stack web developer residing in Portland, Oregon.<br /><br />I have a Bachelor's in Journalism and a Minor in Spanish from the University of Oregon. We built a website back in one of my Journalism courses at the UofO, and I've loved coding ever since!<br></br>I just successfully completed the University of Oregon’s immersive, 24-week Full-Stack Coding Bootcamp—with training in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Integrations, MongoDB, MySQL, Command Line, Git, and much more. Visit my works and download my resume here on my custom-made site!</p>
+        <p className="card-text about-text">Hi! My name is Randi and I'm a full-stack web developer residing in Portland, Oregon.<br />I have a Bachelor's in Journalism and a Minor in Spanish from the University of Oregon. We built a website back in one of my Journalism courses at the UofO, and I've loved coding ever since!<br />I just successfully completed the University of Oregon’s immersive, 24-week Full-Stack Coding Bootcamp—with training in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Integrations, MongoDB, MySQL, Command Line, Git, and much more. Visit my works and download my resume here on my custom-made site!</p>
       </div>
     </div>
   </div>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -14,9 +14,9 @@ export default function Contact() {
 <div className="card-body contact-list res-body">
         <li><AiOutlineMail /> randibrown21@gmail.com</li>
         <li><AiFillPhone /> 503-320-2723</li> 
-        <li><FaGithub /> <a href="https://github.com/randirose">https://github.com/randirose</a></li>
+        <li><FaGithub /> <a href="https://github.com/randirose" className="contact-links">https://github.com/randirose</a></li>
   </div>
-  <p>Check out my skills and download my resume on the 'Resume' tab. <a href='./MirandaBrown.pdf' download>Download my Certificate of Completion</a> from my full-stack bootcamp and <a href='./BCGradesWithAvgs-comments.pdf' download>check out my grades and comments here.</a></p>
+  <p>Check out my skills and download my resume on the 'Resume' tab. <a href='./MirandaBrown.pdf' className="contact-links" download>Download my Certificate of Completion</a> from my full-stack bootcamp and <a href='./BCGradesWithAvgs-comments.pdf' className="contact-links" download>check out my grades and comments here.</a></p>
 </div>
 
 
